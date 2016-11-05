3d26d144865f25b4bd5af81f2d08004977bfe98d
Ben Mitchell <rnaodm5@gmail.com>
2016-11-06 00:19:14 +1300

    Implement the RenderState class which works in conjunction with the DrawableNode objects.

--- include/render_state.hpp
+++ include/render_state.hpp
@@ -1,16 +1,32 @@
 #pragma once
 #include <linkage.hpp>
-
-// TODO: Add includes for all of the state types. E.g. shaders and textures.
+#include <matrix.hpp>
+#include <shader.hpp>
 
 namespace tk {
     namespace graphics {
 
         class TK_GRAPHICS RenderState {
+            class Impl;
+            Impl* impl;
 
         public:
 
             RenderState(); 
+            ~RenderState();
+
+            // Matrix Transforms
+            void pushMatrix();
+            void popMatrix();
+
+            void setMatrix(const core::Mat4f& matrix);
+            void transform(const core::Mat4f& matrix);
+
+            const core::Mat4f& getTransform() const;
+
+            // Shader
+            void setShader(Shader* shader);
+            Shader* getShader() const;
 
         };
 
--- include/scene_graph.hpp
+++ include/scene_graph.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <node.hpp>
 #include <log.hpp>
+#include <render_state.hpp>
+#include <shape.hpp>
 
 namespace tk {
     namespace graphics {
@@ -8,28 +10,50 @@ namespace tk {
         class IDrawable {
         public:
             virtual ~IDrawable() { }
-            virtual void draw() { }
+            virtual void draw(RenderState& state) { }
         };
 
 
         class IDrawableNode : public core::Node<IDrawable> {
         public:
             IDrawableNode(const std::string& name) : core::Node<IDrawable>(name) { }
-            virtual void draw() {
+            virtual void draw(RenderState& state) {
                 for (auto& child : children) {
-                    child->draw();
+                    child->draw(state);
                 }
             }
         };
 
 
-        class TranslateNode : public IDrawableNode {
+        class TransformNode : public IDrawableNode {
+            core::Mat4f matrix;
         public:
-            TranslateNode(const std::string& name) : IDrawableNode(name) { }
-            void draw() {
-                tk_info(core::format("Hello World %%", name));
+            TransformNode(const std::string& name, const core::Mat4f& m = core::Mat4f()) : IDrawableNode(name), matrix(m) { }
+            void draw(RenderState& state) {
+                state.pushMatrix();
+                state.transform(matrix);
 
-                IDrawableNode::draw();
+                IDrawableNode::draw(state);
+                state.popMatrix();
+            }
+
+            core::Mat4f& getMatrix() {
+                return matrix;
+            }
+        };
+
+
+        class ShapeNode : public IDrawableNode {
+            Shape& shape;
+            core::Vec4f tint;
+        public:
+            ShapeNode(const std::string& name, Shape& shape, const core::Vec4f& tint = { 1, 1, 1, 1 }) : IDrawableNode(name), shape(shape), tint(tint) { }
+            void draw(RenderState& state) {
+                state.getShader()->setUniform("transform", state.getTransform());
+                state.getShader()->setUniform("tint", tint);
+                shape.draw();
+
+                IDrawableNode::draw(state);
             }
         };
 
