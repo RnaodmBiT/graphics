4df06aba4ab46de8b844ad46d4d7f8333f842ea6
Ben Mitchell <rnaodm5@gmail.com>
2016-12-21 18:32:25 +1300

    Load a texture from file. This makes use of the stb library. Textures now have selectable wrap modes too

--- include/texture.hpp
+++ include/texture.hpp
@@ -1,18 +1,27 @@
 #pragma once
 
 #include <linkage.hpp>
+#include <resource.hpp>
+#include <vector.hpp>
 #include <GL/glew.h>
 
 namespace tk {
     namespace graphics {
 
-        class TK_GRAPHICS Texture {
+        class TK_GRAPHICS Texture : public core::IResource {
             GLuint object;
             GLenum target;
+            int textureWidth, textureHeight;
 
             void deleteTexture();
 
         public:
+
+            enum WrapMode {
+                Wrap = 0,
+                Clamp = 1,
+                Mirror = 2,
+            };
 
             Texture(GLenum target);
             ~Texture();
@@ -20,10 +29,15 @@ namespace tk {
             Texture(Texture&& move);
             void operator=(Texture&& move);
 
+            static IResource* loadFromFile(const std::string& filename);
+
             void bind() const;
 
             template <class T>
             void setData(T* data, int width, int height, GLenum internalFormat, GLenum format, GLenum type) {
+                textureWidth = width;
+                textureHeight = height;
+
                 bind();
                 glTexImage2D(target, 0, internalFormat, width, height, 0, format, type, data);
                 glGenerateMipmap(target);
@@ -35,7 +49,11 @@ namespace tk {
                 glTexParameteri(target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             }
 
+            void setWrapMode(WrapMode x, WrapMode y);
+
             void useRedAsAlpha();
+
+            core::Vec2i getSize() const;
         };
 
     }
--- src/texture.cpp
+++ src/texture.cpp
@@ -1,6 +1,9 @@
 #include <texture.hpp>
 #include <algorithm>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
 namespace tk {
     namespace graphics {
 
@@ -25,14 +28,36 @@ namespace tk {
             std::swap(target, move.target);
         }
 
+        core::IResource* Texture::loadFromFile(const std::string& filename) {
+            int width, height, components;
+            stbi_uc* data = stbi_load(filename.c_str(), &width, &height, &components, 4);
+            tk_assert(data, core::format("Error loading image file %%", filename));
+
+            Texture* texture = new Texture(GL_TEXTURE_2D);
+            texture->setData(data, width, height, GL_RGBA, GL_RGBA, GL_UNSIGNED_BYTE);
+            return texture;
+        }
+
         void Texture::bind() const {
             glBindTexture(target, object);
+        }
+
+        void Texture::setWrapMode(WrapMode x, WrapMode y) {
+            static GLenum modes[] = { GL_REPEAT, GL_CLAMP_TO_EDGE, GL_MIRRORED_REPEAT };
+
+            bind();
+            glTexParameteri(target, GL_TEXTURE_WRAP_S, modes[x]);
+            glTexParameteri(target, GL_TEXTURE_WRAP_T, modes[y]);
         }
 
         void Texture::useRedAsAlpha() {
             bind();
             GLint swizzle[] = { GL_ONE, GL_ONE, GL_ONE, GL_RED };
             glTexParameteriv(target, GL_TEXTURE_SWIZZLE_RGBA, swizzle);
+        }
+
+        core::Vec2i Texture::getSize() const {
+            return{ textureWidth, textureHeight };
         }
 
     }
